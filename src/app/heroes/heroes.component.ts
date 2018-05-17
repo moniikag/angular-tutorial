d9fd9fc80321a773a936fc008d0e27fbea51713b
Monika Glier <monikaglier@gmail.com>
2018-05-17 11:29:03 +0200

    Link to hero detail from dashboard & heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,22 +10,14 @@ import { Hero } from '../hero'
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-
   constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes)
-  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
+  }
 }

8b578a11bb09949ffd4fe26a2df2890ec05b1754
Monika Glier <monikaglier@gmail.com>
2018-05-17 15:41:11 +0200

    Handle request errors

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
@@ -17,14 +17,18 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes')
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      )
   }
 
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log`fetched hero id=${id}`)
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
     )
   }
 
@@ -33,4 +37,12 @@ export class HeroService {
   }
 
   private heroesUrl = 'api/heroes';
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error)
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
 }

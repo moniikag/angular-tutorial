e7c35e920c160975bad911ac48a9971189a148ef
Monika Glier <monikaglier@gmail.com>
2018-05-17 15:51:08 +0200

    Enable editing a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -29,6 +33,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log`fetched hero id=${id}`)
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  }
+
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     )
   }
 
